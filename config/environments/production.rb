7f9989ec8511dbf5c4580495d3a3c477abec1202
siddhantshah <siddhantnexus@gmail.com>
2017-10-05 00:07:35 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,15 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "CarRentalApp_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'caronerent.herokuapp.com',
+      user_name: 'carrentapplication@gmail.com',
+      password: 'tansidaish',
+      authentication: 'plain',
+      enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
